583640e4b9a05369fd1bae65c92bf00c1ced3cfe
Viththagan R.N. <viththagan1999@gmail.com>
2025-03-08 17:42:57 +0530

    key log testing

--- src/lib/gemini.ts
+++ src/lib/gemini.ts
@@ -1,5 +1,6 @@
 import { GoogleGenerativeAI } from '@google/generative-ai';
 
+console.log("Test Key",import.meta.env.VITE_GEMINI_API_KEY)
 const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
 export interface Software {
   id: string;
