eb2c93d43602794eb019a9f02a61f24fac4b1d18
Viththagan R.N. <viththagan1999@gmail.com>
2025-03-08 17:37:06 +0530

    modified base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/software-recommender/', // Replace with your GitHub repository name
+  base: '/software-recommender/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
