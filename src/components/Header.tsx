c1a4e468a9a2c501f711e58c7017b964a24992e0
Viththagan R.N. <viththagan1999@gmail.com>
2025-03-10 22:32:08 +0530

    removed unused codes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,24 +23,6 @@ export function Header({ onLogoClick }: HeaderProps) {
               <Info className="h-5 w-5 mr-1" />
               <span>About</span>
             </Link>
-            {/*<a*/}
-            {/*  href="https://rnviththagan.github.io/me"*/}
-            {/*  target="_blank"*/}
-            {/*  rel="noopener noreferrer"*/}
-            {/*  className="flex items-center text-gray-600 hover:text-blue-600 transition-colors"*/}
-            {/*>*/}
-            {/*  <Globe className="h-5 w-5 mr-1" />*/}
-            {/*  <span>Portfolio</span>*/}
-            {/*</a>*/}
-            {/*<a*/}
-            {/*  href="https://github.com/RNViththagan"*/}
-            {/*  target="_blank"*/}
-            {/*  rel="noopener noreferrer"*/}
-            {/*  className="flex items-center text-gray-600 hover:text-blue-600 transition-colors"*/}
-            {/*>*/}
-            {/*  <Github className="h-5 w-5 mr-1" />*/}
-            {/*  <span>GitHub</span>*/}
-            {/*</a>*/}
           </div>
         </div>
       </div>
